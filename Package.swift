93be01c70c56209e8984fcc5bc7b77a294a56e75
Yim Lee <yim_lee@apple.com>
2022-08-05 13:34:56 -0700

    Use version-specific manifest for older Swift versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 
 import PackageDescription
 
@@ -9,29 +9,26 @@ let package = Package(
         .library(name: "LifecycleNIOCompat", targets: ["LifecycleNIOCompat"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-metrics.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/swift-server/swift-backtrace.git", from: "1.1.1"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"), // used in tests
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
-    targets: []
+    targets: [
+        .target(name: "Lifecycle", dependencies: [
+            .product(name: "Atomics", package: "swift-atomics"),
+            .product(name: "Logging", package: "swift-log"),
+            .product(name: "Metrics", package: "swift-metrics"),
+            .product(name: "Backtrace", package: "swift-backtrace"),
+        ]),
+
+        .target(name: "LifecycleNIOCompat", dependencies: [
+            "Lifecycle",
+            .product(name: "NIO", package: "swift-nio"),
+        ]),
+
+        .testTarget(name: "LifecycleTests", dependencies: ["Lifecycle", "LifecycleNIOCompat"]),
+    ]
 )
-
-#if compiler(>=5.3)
-package.dependencies += [
-    .package(url: "https://github.com/apple/swift-atomics.git", from: "1.0.0"),
-]
-package.targets += [
-    .target(name: "Lifecycle", dependencies: ["Logging", "Metrics", "Backtrace", "Atomics"]),
-]
-#else
-package.targets += [
-    .target(name: "CLifecycleHelpers", dependencies: []),
-    .target(name: "Lifecycle", dependencies: ["CLifecycleHelpers", "Logging", "Metrics", "Backtrace"]),
-]
-#endif
-
-package.targets += [
-    .target(name: "LifecycleNIOCompat", dependencies: ["Lifecycle", "NIO"]),
-    .testTarget(name: "LifecycleTests", dependencies: ["Lifecycle", "LifecycleNIOCompat"]),
-]
---
+++ scripts/preview_docc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftServiceLifecycle open source project
+##
+## Copyright (c) 2022 Apple Inc. and the SwiftServiceLifecycle project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftServiceLifecycle project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift Distributed Actors open source project
+##
+## Copyright (c) 2018-2019 Apple Inc. and the Swift Distributed Actors project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.md for the list of Swift Distributed Actors project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
+xcrun swift package --disable-sandbox preview-documentation --target $1
