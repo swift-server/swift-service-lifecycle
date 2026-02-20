ae13971145539d64720cafb10215d0c00fff20a9
Gus Cairo <gus.cairo@apple.com>
2026-02-20 10:55:32 +0000

    Add a ClosureService
    You can use ClosureService to create a lightweight, inline Service without defining and conforming a dedicated type.

---
+++ Sources/ServiceLifecycle/ClosureService.swift
@@ -0,0 +1,45 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftServiceLifecycle open source project
+//
+// Copyright (c) 2026 Apple Inc. and the SwiftServiceLifecycle project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftServiceLifecycle project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+/// A service that runs an asynchronous throwing closure as its implementation.
+///
+/// Use `ClosureService` to create a lightweight ``Service`` without defining
+/// a dedicated type. The closure you provide is executed when ``run()`` is called.
+///
+/// ```swift
+/// let service = ClosureService {
+///     // Do some async work
+///     try await foo()
+/// }
+///
+/// // Run the service as normal
+/// try await service.run()
+/// ```
+public struct ClosureService: Service {
+    private let closure: @Sendable () async throws -> Void
+
+    /// Creates a new closure-based service.
+    ///
+    /// - Parameter closure: The asynchronous throwing closure to execute when the service is run.
+    public init(_ closure: @escaping @Sendable () async throws -> Void) {
+        self.closure = closure
+    }
+
+    /// Runs the service by executing the closure provided at initialization.
+    ///
+    /// This method awaits the result of the closure and propagates any errors it throws.
+    public func run() async throws {
+        try await closure()
+    }
+}
