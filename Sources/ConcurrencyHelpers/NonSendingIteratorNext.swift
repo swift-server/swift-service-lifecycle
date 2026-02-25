605e0f626747345110468860cce996e3d6a32abb
Hamzah Malik <hamzah_malik@apple.com>
2026-02-25 11:36:18 +0000

    Docs fix

--- Sources/ConcurrencyHelpers/NonSendingIteratorNext.swift
+++ Sources/ConcurrencyHelpers/NonSendingIteratorNext.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftServiceLifecycle open source project
+//
+// Copyright (c) 2026 Apple Inc. and the SwiftServiceLifecycle project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftServiceLifecycle project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 extension AsyncIteratorProtocol {
     @usableFromInline
